3ca06ce5b292f6bb40bc65cc4432409e28be476e
ivanmijacika <imijacika20@stuy.edu>
2020-12-09 13:22:01 -0500

    first attempt of both constructors added

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,4 +2,11 @@ import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T>{
 
+    public NoNullArrayList(){
+	super();
+    }
+    public NoNullArrayList(int startingCapacity){
+	super(startingCapacity);
+    }
+
 }
