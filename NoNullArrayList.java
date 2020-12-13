8ec587ddc29983c6ca539648a174fbcde4a3059a
ivanmijacika <imijacika20@stuy.edu>
2020-12-12 23:40:09 -0500

    add(index) overriden

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -19,4 +19,9 @@ public class NoNullArrayList<T> extends ArrayList<T>{
 	else return super.add(element);
     }
 
+    public void add(int index, T element){
+	if (element == null) throw new IllegalArgumentException("Null is not a valid element.");
+	else super.add(index, element);
+    }
+
 }
