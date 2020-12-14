ccec05884a0187e8787f0a43dd840c71de724231
ivanmijacika <imijacika20@stuy.edu>
2020-12-14 00:31:23 -0500

    find fixed to throw exception

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,6 +9,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     }
 
     private int find(T element){//helper for add
+	if (element == null) throw new IllegalArgumentException("null is not a valid element");
 	int i = 0;
 	while(i<size() && element.compareTo(get(i))>0) i++;
 	return i;
